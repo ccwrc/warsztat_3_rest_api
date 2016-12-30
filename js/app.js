f887a76dc29f56d8af37c651e8124af57069bfec
ccwrc <j.muszelik@gmail.com>
2016-12-30 16:57:14 +0100

    small cosmetic fixes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     
     var endpoint = "http://localhost/warsztat_3_rest_api/api/books.php";
     // var endpoint = window.location + '/api/books.php'; // not work on localhost
-    
-    addNewBook();
+
+    addEventToCreateNewBook();
     showAllBooks();
     
     function showBookDetails() {
@@ -17,20 +17,20 @@ $(document).ready(function() {
             type: 'GET',
             dataType: 'json'
         })
-                .done(function(json) {
-                    var bookDetails = json; 
-                    display.append("<div class='displayBook'> Autor: " + bookDetails.bookauthor + "<br/> Opis: " 
+                .done(function (json) {
+                    var bookDetails = json;
+                    display.append("<div class='displayBook'> Autor: " + bookDetails.bookauthor + "<br/> Opis: "
                             + bookDetails.bookdescription + '<form class="editBook"> <strong>Edycja książki:</strong> \n\
                             <br/>' + '<input size="100" type="text" name="title" value="' + bookDetails.booktitle +
-                            '"/><br/>' + '<input size="100" type="text" name="author" value="' 
-                            + bookDetails.bookauthor + '"/><br/>' + '<input size="100" type="text" name="description" value="' 
+                            '"/><br/>' + '<input size="100" type="text" name="author" value="'
+                            + bookDetails.bookauthor + '"/><br/>' + '<input size="100" type="text" name="description" value="'
                             + bookDetails.bookdescription + '"/><br/>' + '<input type="submit" \n\
                             class="edit" value="Edytuj książkę"\n\
                             name="' + bookDetails.bookid + '"/></form> <button class="delete" \n\
-                            value="' + bookDetails.bookid + '">Lub usuń książkę</button><br/></div><br/>' );
-                    
+                            value="' + bookDetails.bookid + '">Lub usuń książkę</button><br/></div><br/>');
+
                     $('.delete').click(deleteBook);
-                    $('.editBook').on('submit', function(e) {
+                    $('.editBook').on('submit', function (e) {
                         e.preventDefault();
                         editBook(this)
                     });
@@ -46,10 +46,11 @@ $(document).ready(function() {
             type: "DELETE",
             dataType: "json"
         })
-                .done(function(json) {
+                .done(function (json) {
+                    alert("Książka usunięta");
                     location.reload(true);
                 })
-                .fail(function() {
+                .fail(function () {
                     alert("Książka się opiera, spróbuj później");
                 });
     }
@@ -68,16 +69,16 @@ $(document).ready(function() {
             type: "PUT",
             dataType: "json"
         })
-                .done(function(json) {
+                .done(function (json) {
                     alert("Zapisano edytowane dane");
                     location.reload(true);
                 })
-                .fail(function() {
+                .fail(function () {
                     alert("Książka się opiera, spróbuj później");
                 });
     }
     
-    function addNewBook() {
+    function addEventToCreateNewBook() {
         $('form[name="addBook"]').on('submit', function(e) {
             e.preventDefault();
 
@@ -93,11 +94,11 @@ $(document).ready(function() {
                 type: "POST",
                 dataType: "json"
             })
-                    .done(function(json) {
+                    .done(function (json) {
                         alert("Książka dodana");
                         window.location.reload(true);
                     })
-                    .fail(function() {
+                    .fail(function () {
                         alert("Błąd dodawania książki, spróbuj ponownie później");
                     });
         });
@@ -110,9 +111,9 @@ $(document).ready(function() {
             type: "GET",
             dataType: "json"
         })
-                .done(function(json) { // http://mozillapl.org/forum/viewtopic.php?t=31798
+                .done(function (json) { // http://mozillapl.org/forum/viewtopic.php?t=31798
                     var books = json;
-                    $.each(books, function(object, book) {
+                    $.each(books, function (object, book) {
                         var book = $("<div id='" + book.bookid + "''class='displayBook'>" + book.booktitle
                                 + "</div>");
                         $('.booksList').append(book);
@@ -122,7 +123,7 @@ $(document).ready(function() {
                     title.one('click', showBookDetails);
                 })
 
-                .fail(function() {
+                .fail(function () {
                     alert("Baza książek jest pusta");
                 });
     }            

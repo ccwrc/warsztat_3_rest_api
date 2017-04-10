377136e2a11bac773b07c43b9f4aac296f1a6389
ccwrc <j.muszelik@gmail.com>
2017-04-10 13:54:23 +0200

    book.php - refactor

--- api/books.php
+++ api/books.php
@@ -6,31 +6,31 @@ require_once 'src/connect.php';
 $conn = getDbConnection();
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if (isset($_GET['id']) && trim($_GET['id']) != ""
-        && is_numeric($_GET['id']) && ($_GET['id'] > 0)) {
+    if (isset($_GET['id']) && trim($_GET['id']) != "" && is_numeric($_GET['id']) 
+            && ($_GET['id'] > 0)) {
         $book = Book::loadFromDbById($conn, $_GET['id']);
         $serializedData = json_encode($book);
         echo $serializedData;
     } else {
         $books = Book::loadAllFromDb($conn);
         $serializedData = json_encode($books);
-        echo $serializedData; 
+        echo $serializedData;
     }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if (isset($_POST['author']) && (trim($_POST['author']) != "")
-       && isset($_POST['title']) && (trim($_POST['title']) != "")
-       && isset($_POST['description']) && (trim($_POST['description']) != "")) {
-        
+    if (isset($_POST['author']) && (trim($_POST['author']) != "") && isset($_POST['title']) 
+            && (trim($_POST['title']) != "") && isset($_POST['description']) 
+            && (trim($_POST['description']) != "")) {
+
         $author = $_POST['author'];
         $title = $_POST['title'];
         $description = $_POST['description'];
-        
+
         $book = new Book();
         $book->createBook($conn, $author, $title, $description);
         echo json_encode($book);
-       } 
+    }
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
@@ -45,13 +45,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
     $author = $put_vars['author'];
     $title = $put_vars['title'];
     $description = $put_vars['description'];
-    
+
     $book = new Book();
     $book->updateBook($conn, $author, $title, $description, $id);
     echo json_encode($book);
 }
 
 $conn->close();
-$conn = null;  
-
+$conn = null;
 ?>
--- api/src/Book.php
+++ api/src/Book.php
@@ -5,30 +5,30 @@ class Book implements JsonSerializable {
     private $bookTitle;
     private $bookAuthor;
     private $bookDescription;
-    
+
     public function __construct() {
         $this->bookId = -1;
         $this->bookTitle = "";
         $this->bookAuthor = "";
         $this->bookDescription = "";
     }
-    
+
     public function getBookId() {
         return $this->bookId;
     }
-    
+
     public function getBookTitle() {
         return $this->bookTitle;
     }
-    
+
     public function getBookAuthor() {
         return $this->bookAuthor;
     }
-    
+
     public function getBookDescription() {
         return $this->bookDescription;
     }
-    
+
     public function setBookTitle($bookTitle) {
         if (is_string($bookTitle) && (strlen($bookTitle) <= 255)) {
             $this->bookTitle = $bookTitle;
@@ -37,7 +37,7 @@ class Book implements JsonSerializable {
             return false;
         }
     }
-    
+
     public function setBookAuthor($bookAuthor) {
         if (is_string($bookAuthor) && (strlen($bookAuthor) <= 255)) {
             $this->bookAuthor = $bookAuthor;
@@ -46,7 +46,7 @@ class Book implements JsonSerializable {
             return false;
         }
     }
-    
+
     public function setBookDescription($bookDescription) {
         if (is_string($bookDescription) && (strlen($bookDescription) <= 25000)) {
             $this->bookDescription = $bookDescription;
@@ -55,15 +55,15 @@ class Book implements JsonSerializable {
             return false;
         }
     }
-    
+
     static public function loadFromDbById(mysqli $conn, $id) {
-        $id = htmlentities($id, ENT_QUOTES, "UTF-8");
-        $id = $conn->real_escape_string($id);
-        
+        $id = $conn->real_escape_string(htmlentities($id, ENT_QUOTES, "UTF-8"));
+
         $sql = "SELECT book_author, book_title, book_description "
                 . "FROM book WHERE book_id = $id";
-        if ($res = $conn->query($sql)) {
-            $row = $res->fetch_assoc();
+        $result = $conn->query($sql);
+        if ($result) {
+            $row = $result->fetch_assoc();
             $book = new Book();
             $book->bookAuthor = $row['book_author'];
             $book->bookTitle = $row['book_title'];
@@ -72,12 +72,12 @@ class Book implements JsonSerializable {
             return $book;
         } else {
             return false;
-        } 
+        }
     }
-    
+
     static public function loadAllFromDb(mysqli $conn) {
         $books = [];
-        
+
         $sql = "SELECT * FROM book";
         $result = $conn->query($sql);
         if ($result && $result->num_rows > 0) {
@@ -94,40 +94,31 @@ class Book implements JsonSerializable {
             return false;
         }
     }
-    
+
     public function createBook(mysqli $conn, $author, $title, $description) {
-        $author = htmlentities($author, ENT_QUOTES, "UTF-8");
-        $title = htmlentities($title, ENT_QUOTES, "UTF-8");
-        $description = htmlentities($description, ENT_QUOTES, "UTF-8");
-        
-        $author = $conn->real_escape_string($author);
-        $title = $conn->real_escape_string($title);
-        $description = $conn->real_escape_string($description);
-        
+        $author = $conn->real_escape_string(htmlentities($author, ENT_QUOTES, "UTF-8"));
+        $title = $conn->real_escape_string(htmlentities($title, ENT_QUOTES, "UTF-8"));
+        $description = $conn->real_escape_string(htmlentities($description, ENT_QUOTES, "UTF-8"));
+
         $sql = "INSERT INTO book (book_author, book_title, book_description)"
                 . " VALUES ('$author', '$title', '$description')";
         if ($conn->query($sql)) {
             $this->bookAuthor = $author;
             $this->bookTitle = $title;
             $this->bookDescription = $description;
-            $this->bookId = $conn->insert_id; 
+            $this->bookId = $conn->insert_id;
             return true;
         } else {
             return false;
         }
     }
-    
+
     public function updateBook(mysqli $conn, $author, $title, $description, $id) {
-        $author = htmlentities($author, ENT_QUOTES, "UTF-8");
-        $title = htmlentities($title, ENT_QUOTES, "UTF-8");
-        $description = htmlentities($description, ENT_QUOTES, "UTF-8");
-        $id = htmlentities($id, ENT_QUOTES, "UTF-8");
-        
-        $author = $conn->real_escape_string($author);
-        $title = $conn->real_escape_string($title);
-        $description = $conn->real_escape_string($description);
-        $id = $conn->real_escape_string($id);
-        
+        $author = $conn->real_escape_string(htmlentities($author, ENT_QUOTES, "UTF-8"));
+        $title = $conn->real_escape_string(htmlentities($title, ENT_QUOTES, "UTF-8"));
+        $description = $conn->real_escape_string(htmlentities($description, ENT_QUOTES, "UTF-8"));
+        $id = $conn->real_escape_string(htmlentities($id, ENT_QUOTES, "UTF-8"));
+
         $sql = "UPDATE book SET book_author = '$author', book_title = '$title', "
                 . "book_description = '$description' WHERE book_id = $id";
         if ($conn->query($sql)) {
@@ -139,28 +130,25 @@ class Book implements JsonSerializable {
             return false;
         }
     }
-    
+
     static public function deleteFromDb(mysqli $conn, $id) {
-        $id = htmlentities($id, ENT_QUOTES, "UTF-8");
-        $id = $conn->real_escape_string($id);
-        
+        $id = $conn->real_escape_string(htmlentities($id, ENT_QUOTES, "UTF-8"));
+
         $sql = "DELETE FROM book WHERE book_id = $id";
-        if ($res = $conn->query($sql)) {
+        if ($conn->query($sql)) {
             return true;
         } else {
             return false;
         }
     }
-    
-    public function jsonSerialize() {
-       return [
-          'booktitle' => $this->bookTitle,
-          'bookauthor' => $this->bookAuthor,
-          'bookdescription' => $this->bookDescription,
-          'bookid' => $this->bookId 
-       ];
-    } 
-      
-}
 
-?>
+    public function jsonSerialize() {
+        return [
+            'booktitle' => $this->bookTitle,
+            'bookauthor' => $this->bookAuthor,
+            'bookdescription' => $this->bookDescription,
+            'bookid' => $this->bookId
+        ];
+    }
+
+}
